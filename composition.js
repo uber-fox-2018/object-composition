6980b21eb56fb137f43e14f7ee09f1e93aa8454a
andryjns <andry_jns@yahoo.com>
2018-07-17 22:36:46 +0700

    update for release 2 /Andry

--- composition.js
+++ composition.js
@@ -1,11 +1,71 @@
 "use strict"
+
 var fs = require('fs')
+var options = fs.readFileSync('cookies.txt')
+    .toString()
+    .split("\r\n")
+
+var parsingResult= parsingTextCookie(options)  
+    
+function parsingTextCookie(options) {
+    var result=[]
+    var ingredientPerCookie
+ 
+    options.forEach(item => {
+        var separateNameAndIngredients= item.split('=')
+        ingredientPerCookie= parsingIngredients(separateNameAndIngredients[1])
+        var namePerCookie= separateNameAndIngredients[0]
+
+        var wrapping={}
+        wrapping.name= namePerCookie
+        wrapping.options= ingredientPerCookie
+        result.push(wrapping)
+    })
+    
+    return result
+}
+//console.log (parsingTextCookie(options)[0])
+
+
+function parsingIngredients(line){
+    
+    var separateIngredients= line.split(',')
+    var result=[]
+
+    separateIngredients.forEach(item =>{
+        result.push(parsingIngredient(item))
+    }) 
+    return result
+}
+
+function parsingIngredient (ingredientStr) {
+    var separateIngredientsNameAndAmount= ingredientStr.split(':')
+    return {
+    'name': separateIngredientsNameAndAmount[1], 
+    'amount': separateIngredientsNameAndAmount[0]
+    }
+}
+
+
+
+
+
+//============================================================================//
+
+class Ingredient {
+    constructor(options) {
+        this.name= options['name']
+        this.amount= options['amount']
+    }
+}
+
 
 class Cookie {
-    constructor(name) {
-        this.name= name
+    constructor(ingredients) {
+        this.name
         this.status= 'mentah'
-        this.ingredients= []
+        this.ingredients= ingredients
+        this.has_sugar=null
 
     }
 
@@ -39,33 +99,36 @@ class OtherCookie extends Cookie {
 //=========================================================================//
 
 
-var options = fs.readFileSync('cookies.txt')
-    .toString()
-    .split("\r\n")
-  
-
-console.log (options)
-
 class CookieFactory {
     static create(options){
         var result= []
+        
 
         for (let i=0; i<options.length; i++){
-            if (options[i]=='peanut butter') {
+            var ingredients=[]
+            options[i]['options'].forEach(item =>{
+                let ingredient = new Ingredient(item)
+                ingredients.push(ingredient)
+
+            })
+            if (options[i]['name']=='peanut butter') {
                 let peanut_butter= new PeanutButter()
-                peanut_butter.name= options[i]
+                peanut_butter.name= options[i]['name']
+                peanut_butter.ingredients= ingredients
                 result.push(peanut_butter)
             }
 
-            else if (options[i]=='chocolate chip') {
+            else if (options[i]['name']=='chocolate chip') {
                 let chocolate_chip= new ChocolateChip()
-                chocolate_chip.name= options[i]
+                chocolate_chip.name= options[i]['name']
+                chocolate_chip.ingredients= ingredients
                 result.push(chocolate_chip)
             }
             
             else {
                 let other_cookie= new OtherCookie()
-                other_cookie.name= options[i]
+                other_cookie.name= options[i]['name']
+                other_cookie.ingredients= ingredients
                 result.push(other_cookie)
             }
         }
@@ -73,5 +136,13 @@ class CookieFactory {
     }
 }
 
-let batch_of_cookies = CookieFactory.create(options)
+let batch_of_cookies = CookieFactory.create(parsingResult)
 console.log(batch_of_cookies)
+
+/*
+let sugarFreeFoods= CookieFactory.cookieRecommendation('tuesday', batch_of_cookies)
+console.log('sugar free cakes are : ')
+for (let i= 0; i<sugarFreeFoods.length; i++) {
+    console.log(sugarFreeFoods[i].name)
+}
+*/
